245e33bf7508086f2bd91ef0213d99bf024bee58
KrutonS <krutonpl@gmail.com>
2021-12-18 22:04:25 +0100

    finish

--- src/components/header.js
+++ src/components/header.js
@@ -6,6 +6,11 @@ const Header = memo(() => {
       <h1>Portfolio</h1>
       <h2 className='accent-text'>Grafiki</h2>
       <h3>Marcin Smarzewski</h3>
+      <h4>
+        <a className='accent-text' href='https://github.com/krutonS'>
+          https://github.com/krutonS
+        </a>
+      </h4>
     </header>
   );
 });
