c333196070c8a96875527de1c3b0267944e3d0e1
KrutonS <krutonpl@gmail.com>
2021-12-18 20:19:37 +0100

    fin?

--- src/components/select.js
+++ src/components/select.js
@@ -1,5 +1,5 @@
 import cn from "classnames";
-import React, { memo, useState } from "react";
+import React, { memo, useLayoutEffect, useRef, useState } from "react";
 import accessibleClick from "../../utils/accessibleClick";
 
 const Select = memo(
@@ -7,11 +7,31 @@ const Select = memo(
     const [show, setShow] = useState(false);
     const toggle = () => setShow(!show);
     const accessibleToggle = accessibleClick(toggle);
+    const containerRef = useRef(null);
     const onFocus = () => {
       if (!show) setShow(true);
     };
+    useLayoutEffect(() => {
+      const containerHtml = containerRef.current;
+      const onFocusOut = (e) => {
+        e.stopPropagation();
+        setShow(false);
+      };
+      containerHtml.addEventListener("focusout", onFocusOut);
+      return () => {
+        containerHtml.removeEventListener("focusout", onFocusOut);
+      };
+    }, []);
+    const onBlur = (e) => {
+      if (show) setShow(false);
+    };
     return (
-      <nav onFocus={onFocus} className={cn("dropdown-container", className)}>
+      <nav
+        onFocus={onFocus}
+        className={cn("dropdown-container", className)}
+        ref={containerRef}
+        onBlurCapture={onBlur}
+      >
         <span className='dropdown-label' {...accessibleToggle}>
           {label}
         </span>
